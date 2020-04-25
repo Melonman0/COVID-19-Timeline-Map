2637cbc9814df74ebd545b7c0f2e5d6a5b5e6603
Ulid J <ulid@yahoo.com>
2020-04-25 02:00:28 -0400

    Added slider and updated some event listeners

---
+++ CovidData.js
@@ -0,0 +1,12 @@
+let CovidData = function(json, color) {
+    this.json = json;
+    this.color = color;
+    this.mesh = null;           
+}
+
+CovidData.prototype = { 
+    getData : function() {
+        
+    },
+
+}
