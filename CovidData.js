3f3482b387e243c3f5445f1280614ea2d42a6b3f
Ulid J <ulid@yahoo.com>
2020-04-27 19:32:19 -0400

    Clean up and changes made

--- CovidData.js
+++ CovidData.js
@@ -2,11 +2,12 @@ const FIRST_DATA_DATE = new Date("1/22/20");
 const LAST_DATA_DATE = new Date("4/19/20");
 
 let CovidData = function(countryData) {    
+    this.shapeColor = 0xFF0000;
     this.mesh = CovidData.initDataCircle(countryData);  
     this.mesh.userData = this;
 
     this.data = countryData;
-    this.currentCircleScale = 1;
+    this.currentCircleScale = 1;    
 }
 
 CovidData.initDataCircle = function(data) {
@@ -14,38 +15,28 @@ CovidData.initDataCircle = function(data) {
     let mat = new THREE.MeshBasicMaterial({color: 0xFF0000, transparent: true, opacity: 0.4 });    
     let mesh = new THREE.Mesh( geometry, mat ) ;
 
-    //console.log(data)
-    //console.log("country log lat", country.Long, country.Lat);
     let pos = proj.inverse({x: data.Long, y: data.Lat});
-    //console.log("pos", pos);
+    
     mesh.position.x = pos.x;
     mesh.position.y = pos.y;
     mesh.position.z = .1;
-    //mesh.scale.set(10,10,1);    
-
+    
     return mesh;
-    //console.log(mesh.position)
 }
 
 CovidData.prototype = { 
-    getConfirmedCasesFromDate : function(date) {
-        let objDate = new Date(date);
-        let strDate = objDate.toLocaleDateString("en", ({ month:"numeric", day:"numeric", year: "2-digit"}));
-        
-        return this.data[strDate];
+    getConfirmed: function(dateString) {
+        return numberformatter.to(this.data[dateString]);
     },
-    updateScale: function() {
-        let confirmed = this.data[selectedDate];
-        //console.log(date);
-        this.currentCircleScale = Math.log2(confirmed) * 1;
-        //this.mesh.scale.set(scale, scale, 1);
-
+    getInfoString(dateString) {
+        let provState = (this.data["Province/State"]) ? `, ${this.data["Province/State"]}` : "";
+        return `${this.data["Country/Region"]}${provState} - ${this.getConfirmed(dateString)}`;
     },
-    updateCircle: function() {
-        this.mesh.scale.set(this.currentCircleScale, this.currentCircleScale, 1);
+    updateCircle: function(dateString) {
+        let confirmed = this.data[dateString];
 
-        
-    }
-    
-
+        this.currentCircleScale = Math.log(confirmed)/Math.log(3);
+        //this.currentCircleScale = confirmed/10000;
+        this.mesh.scale.set(this.currentCircleScale, this.currentCircleScale, 1);                
+    }    
 }
