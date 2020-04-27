2147422ed132869b686577456a9c30c119911ce3
Ulid J <ulid@yahoo.com>
2020-04-26 22:08:44 -0400

    Added slider

--- CovidData.js
+++ CovidData.js
@@ -1,3 +1,6 @@
+const FIRST_DATA_DATE = new Date("1/22/20");
+const LAST_DATA_DATE = new Date("4/19/20");
+
 let CovidData = function(json, color) {
     this.json = json;
     this.color = color;
@@ -5,8 +8,11 @@ let CovidData = function(json, color) {
 }
 
 CovidData.prototype = { 
-    getData : function() {
+    getDataFromDate : function(date) {
+        let objDate = new Date(date);
+        let strDate = objDate.toLocaleDateString("en", ({ month:"numeric", day:"numeric", year: "2-digit"}));
         
+        return json[strDate];
     },
 
 }
